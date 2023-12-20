a91c15b8a813197e6e28e149d94d1150d30426f6
Roan Richmond <roan.richmond@codethink.co.uk>
2023-12-20 08:33:27 +0000

    Problem 2706. Finished
    Signed-off-by: Roan Richmond <roan.richmond@codethink.co.uk>

---
+++ C++/Problem 2706. Buy Two Chocolates.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int buyChoco(vector<int>& prices, int money) 
+	{
+		sort(prices.begin(), prices.end());
+
+
+		if ((money - prices[0] - prices[1]) >= 0) 
+			return money - prices[0] - prices[1];
+		return money; 
+	}
+};
